2883e43a7410b80d0b150cf21e938b3b4aed8373
pnhung177 <pnhung177@drupalex.net>
2015-12-07 23:59:56 +0700

    Implements the getDependencies() based on argumentSchema

--- index.js
+++ index.js
@@ -1,61 +1,76 @@
 var InjektorClass = function InjektorClass() {
-    
+
   var dependencies = {};
 
   /**
    * Get the dependency. If the call is the first time, copy it (for object) or
    * instantiate if (for constructor).
    */
-  var retrieve = function(name) {
+  function retrieve(name) {
     var record = dependencies[name];
     if (record == null) {
-        throw new Error('No service registered with name: '+name);
+        throw new Error('No service registered with name: ' + name);
     }
     if (record['cache'] == null) convert(name);
     return record['cache'];
-  };
+  }
   
   /**
    * Converts the dependency from definition into an instance (object or service).
    */
-  var convert = function(name) {
+  function convert(name) {
     var record = dependencies[name];
     if (record['type'] == 'service') {
       record['cache'] = instantiate(record['value']);
     } else {
       record['cache'] = record['value'];
     }
-  };
+  }
   
   /**
    * Instantiates a constructor
    */
-  var instantiate = function(fn) {
+  function instantiate(fn) {
     var wrapper = function(args) {
       return fn.apply(this, args);
     };
     wrapper.prototype = fn.prototype;
     return new wrapper(getDependencies(fn));
-  };
+  }
   
   /**
    * Extracts the dependencies of a constructor
    */
-  var getDependencies = function(fn) {
-    var params = getParameters(fn);
-    return params.map(function(value) {
-      return retrieve(value);
-    });
-  };
+  function getDependencies(fn) {
+    if (fn.argumentSchema) {
+      var schema = fn.argumentSchema;
+      if (schema instanceof Object && !(schema instanceof Array) &&
+          schema.properties instanceof Object && !(schema.properties instanceof Array)) {
+        var paramObject = {};
+        var paramNames = Object.keys(schema.properties || {});
+        for(var i=0; i<paramNames.length; i++) {
+          paramObject[paramNames[i]] = retrieve(paramNames[i]);
+        }
+        return [paramObject];
+      } else {
+        throw new Error('constructor_has_invalid_argumentSchema');
+      }
+    } else {
+      var params = getParameters(fn);
+      return params.map(function(value) {
+        return retrieve(value);
+      });
+    }
+  }
 
-  var getParameters = function(fn) {
+  function getParameters(fn) {
     var args = fn.toString()
       .replace(/((\/\/.*$)|(\/\*[\s\S]*?\*\/)|(\s))/mg,'')
       .match(/^function\s*[^\(]*\(\s*([^\)]*)\)/m)[1]
       .split(/,/);
     if (args.length == 1 && args[0] == "") return [];
     return args;
-  };
+  }
 
   /**
    * Registering an object as a dependency
@@ -89,8 +104,5 @@ var InjektorClass = function InjektorClass() {
 
   return this;
 };
-
-InjektorClass.ARGUMENTS_IN_JSON = 1;
-InjektorClass.ARGUMENTS_IN_LIST = 2;
 
 module.exports = InjektorClass;
