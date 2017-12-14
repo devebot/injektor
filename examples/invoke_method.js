21a76f6c908d5cab112261bc6f0fb4715b6b1907
pnhung177 <pnhung177@acegik.net>
2017-12-15 03:29:05 +0700

    Support service inline dependency annotation

--- examples/invoke_method.js
+++ examples/invoke_method.js
@@ -4,15 +4,17 @@ var Injektor = require('../index.js');
 var injektor = new Injektor();
 
 injektor
-  .defineService('recipe', function(steps, object) {
-    steps = steps || [];
-    this.action = function(name) {
-      console.log('Hello, the instruction of %s is:', name);
-      steps.forEach(function(step) {
-        console.log(' - %s the %s', step, object);
-      });
-    };
-  });
+  .defineService('recipe', ['steps', 'object',
+    function(steps, object) {
+      steps = steps || [];
+      this.action = function(name) {
+        console.log('Hello, the instruction of %s is:', name);
+        steps.forEach(function(step) {
+          console.log(' - %s the %s', step, object);
+        });
+      };
+    }
+  ]);
 
 injektor
   .registerObject('steps', [
