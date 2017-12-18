68064385e7d4448586a661338d942c9431504af2
pnhung177 <pnhung177@acegik.net>
2017-12-18 08:42:14 +0700

    Lookup bean by name with scope support

--- test/tdd/service_argument_fields-test.js
+++ test/tdd/service_argument_fields-test.js
@@ -83,4 +83,143 @@ describe('defineService:', function() {
 			debugx.enabled && debugx('Message: %s', msg);
 		});
 	});
+
+	describe('service_argument_fields_with_scope', function() {
+		var injektor, MyAction;
+		var TopParameter1, TopParameter2, TopSub1, TopSub2, TopSub3;
+		var ChildParameter1, ChildParameter2, ChildSub1, ChildSub2;
+
+		before(function() {
+			injektor = new Injektor();
+			MyAction = function(params) {
+				this.injected = params;
+			};
+
+			MyAction.argumentProperties = ['name', 'payload', 'parameter1', 'child/parameter2'];
+
+			TopParameter1 = function(params) {
+				this.name = "top/parameter1";
+				this.argumentProperties = TopParameter1.argumentProperties;
+				this.injected = params;
+			}
+			TopParameter1.argumentProperties = ['sub1', 'child/sub2']
+
+			TopParameter2 = function(params) {
+				this.name = "top/parameter2";
+				this.argumentProperties = TopParameter2.argumentProperties;
+				this.injected = params;
+			}
+			TopParameter2.argumentProperties = ['child/sub1', 'sub2']
+
+			TopSub1 = function(params) {
+				this.name = "top/sub1";
+			}
+			TopSub1.argumentProperties = [];
+
+			TopSub2 = function(params) {
+				this.name = "top/sub2";
+			}
+			TopSub2.argumentProperties = [];
+
+			TopSub3 = function(params) {
+				this.name = "top/sub3";
+			}
+			TopSub3.argumentProperties = [];
+
+			ChildParameter1 = function(params) {
+				this.name = "child/parameter1";
+				this.argumentProperties = ChildParameter1.argumentProperties;
+				this.injected = params;
+			}
+			ChildParameter1.argumentProperties = ['top/sub1', 'sub2']
+
+			ChildParameter2 = function(params) {
+				this.name = "child/parameter2";
+				this.argumentProperties = ChildParameter2.argumentProperties;
+				this.injected = params;
+			}
+			ChildParameter2.argumentProperties = ['sub1', 'top/sub2', 'sub3']
+
+			ChildSub1 = function() {
+				this.name = "child/sub1";
+			}
+
+			ChildSub2 = function() {
+				this.name = "child/sub2";
+			}
+		});
+
+		it('lookup a service with name resolution using scopes', function() {
+			injektor
+				.defineService('top/myAction', MyAction)
+				.registerObject('name', 'Peter Pan')
+				.registerObject('payload', {
+					type: 'Book',
+					content: 'Story about Peter and Wendy',
+					price: 17.7
+				})
+				.defineService('child/sub1', ChildSub1)
+				.defineService('child/sub2', ChildSub2)
+				.defineService('child/parameter1', ChildParameter1)
+				.defineService('child/parameter2', ChildParameter2)
+				.defineService('top/parameter1', TopParameter1)
+				.defineService('top/parameter2', TopParameter2)
+				.defineService('top/sub1', TopSub1)
+				.defineService('top/sub2', TopSub2)
+				.defineService('top/sub3', TopSub3);
+
+			var exceptions = [];
+			var myAction = injektor.lookup('top/myAction', exceptions);
+
+			assert.equal(exceptions.length, 0);
+			assert.isNotNull(myAction);
+
+			console.log(JSON.stringify(myAction, null, 4));
+
+			assert.deepInclude(myAction, {
+				"injected": {
+					"name": "Peter Pan",
+					"payload": {
+						"type": "Book",
+						"content": "Story about Peter and Wendy",
+						"price": 17.7
+					},
+					"parameter1": {
+						"name": "top/parameter1",
+						"argumentProperties": [
+							"sub1",
+							"child/sub2"
+						],
+						"injected": {
+							"sub1": {
+								"name": "top/sub1"
+							},
+							"child/sub2": {
+								"name": "child/sub2"
+							}
+						}
+					},
+					"child/parameter2": {
+						"name": "child/parameter2",
+						"argumentProperties": [
+							"sub1",
+							"top/sub2",
+							"sub3"
+						],
+						"injected": {
+							"sub1": {
+								"name": "child/sub1"
+							},
+							"top/sub2": {
+								"name": "top/sub2"
+							},
+							"sub3": {
+								"name": "top/sub3"
+							}
+						}
+					}
+				}
+			});
+		});
+	});
 });
