98bd77c807bc04d91f11129a881067cff557df79
pnhung177 <pnhung177@acegik.net>
2018-03-01 16:41:58 +0700

    Replace jsonschema by ajv

--- src/validator.js
+++ src/validator.js
@@ -1,16 +1,19 @@
 'use strict';
 
-var Validator = require('jsonschema').Validator;
-var validator = new Validator();
+var Ajv = require('ajv');
 
-var Constructor = function() {
+var Constructor = function(params) {
+  params = params || {};
+
+  var validator = new Ajv(Object.assign({allErrors: true}, params.options));
+
   this.validate = function(data, schema, opts) {
     var output = { ok: true, hasErrors: false };
-    var result = validator.validate(data, schema);
-    if (result.errors.length > 0) {
+    var valid = validator.validate(schema, data);
+    if (!valid) {
       output.ok = false;
       output.hasErrors = true;
-      output.errors = result.errors;
+      output.errors = validator.errors;
     }
     return output;
   }
